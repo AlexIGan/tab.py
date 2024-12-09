796d097b7190253d2b28d12cb8bbe5eb0dbddcfd
AlexIGan <Ganievaaleksandra@gmail.com>
2024-12-09 14:11:33 +0300

    Update tab.py

--- tab.py
+++ tab.py
@@ -1,7 +1,15 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
+print(my_list)
+
 while index < len(my_list):
-    if my_list[index] < 0:
+    i = my_list[index]
+    index = index + 1
+    if i ==0:
+        continue
+    elif i <0:
         break
-    print(my_list[index])
-    index += 1
+    elif index == len(my_list):
+        print(i)
+    else:
+        print(i)
